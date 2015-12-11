68e87c3eb61b0d8f89dd705d7b27ed7858db92bb
Gear61 <chessnone@yahoo.com>
2015-12-10 21:35:52 -0800

    Basic start/abandon race logic

--- app/src/main/java/com/randomappsinc/mathrace/Activities/RaceActivity.java
+++ app/src/main/java/com/randomappsinc/mathrace/Activities/RaceActivity.java
@@ -3,19 +3,33 @@ package com.randomappsinc.mathrace.Activities;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.FontAwesomeIcons;
 import com.randomappsinc.mathrace.R;
 import com.randomappsinc.mathrace.Utils.Constants;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by alexanderchiou on 12/10/15.
  */
 public class RaceActivity extends StandardActivity {
+    public static final String STARTING_TIME = "2:00";
+
+    @Bind(R.id.start_race) View startRace;
+    @Bind(R.id.race_layout) View raceLayout;
+    @Bind(R.id.timer) TextView timer;
+    @Bind(R.id.num_correct) TextView numCorrectView;
+    @Bind(R.id.num_wrong) TextView numWrongView;
+
     private String runType;
+    private int numCorrect;
+    private int numWrong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,21 @@ public class RaceActivity extends StandardActivity {
 
         runType = getIntent().getStringExtra(Constants.RUN_TYPE_KEY);
         setTitle(runType + getString(R.string.race));
+    }
+
+    @OnClick(R.id.start_race)
+    public void startRace(View view) {
+        startFreshRace();
+    }
+
+    public void startFreshRace() {
+        startRace.setVisibility(View.GONE);
+        raceLayout.setVisibility(View.VISIBLE);
+        timer.setText(STARTING_TIME);
+        numCorrect = 0;
+        numWrong = 0;
+        numCorrectView.setText(String.valueOf(numCorrect));
+        numWrongView.setText(String.valueOf(numWrong));
     }
 
     @Override
@@ -41,6 +70,8 @@ public class RaceActivity extends StandardActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.start_new_race) {
+            raceLayout.setVisibility(View.GONE);
+            startRace.setVisibility(View.VISIBLE);
         }
         return super.onOptionsItemSelected(item);
     }
